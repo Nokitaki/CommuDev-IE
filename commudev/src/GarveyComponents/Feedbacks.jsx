c88ef159c9c4387e7637fbb6c4691f2f0c95529f
Nokitaki <joelchandlerp@gmail.com>
2024-11-28 16:30:11 +0800

    jh

--- commudev/src/GarveyComponents/Feedbacks.jsx
+++ commudev/src/GarveyComponents/Feedbacks.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../styles/TaskManagerTest.css";
+import "../styles/FeedbackTest.css";
 import { Link } from "react-router-dom";
 import LogoIcon from "../assets/prof/logo.png";
 import HomeIcon from "../assets/HomeIcon.svg";
@@ -263,7 +263,7 @@ const Feedbacks = () => {
             </div>
           </div>
 
-          <div className="resources-container grid">
+          <div className="feedback-container grid">
             {feedbacks
               .filter(feedback => selectedCategory === "all" || feedback.feedbackType === selectedCategory)
               .map((feedback) => (
