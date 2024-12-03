b9928b5449ac8a362bfbcdec772e07e4f0261bf5
Nokitaki <joelchandlerp@gmail.com>
2024-12-04 02:20:15 +0800

    s

--- commudev/src/GarveyComponents/Rewards.jsx
+++ commudev/src/GarveyComponents/Rewards.jsx
@@ -82,7 +82,7 @@ const Rewards = () => {
   const [isRedeeming, setIsRedeeming] = useState(false);
 
   const navigationItems = [
-    { icon: HomeIcon, label: "Home", path: "/" },
+    { icon: HomeIcon, label: "Home", path: "/newsfeed" },
     { icon: MessageIcon, label: "Messages" },
     { icon: ResourceIcon, label: "Resources", path: "/resource" },
     { icon: TaskIcon, label: "Task", path: "/task" },
@@ -231,7 +231,7 @@ const Rewards = () => {
       {/* Left Sidebar */}
       <div className="sidebar">
         <div className="header">
-          <Link to="/">
+          <Link to="/newsfeed">
             <div className="logo">
               <img src={LogoIcon} alt="Logo" className="logo-icon" />
             </div>
